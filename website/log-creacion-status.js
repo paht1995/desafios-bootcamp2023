a48d900f4e2f4fc3b384ebaffa0404f8d6dd1544
paht1995 <paht1995@gmail.com>
2023-06-25 23:27:45 -0300

    Creación de log para eliminación de status.js

---
+++ website/log-creacion-status.js
@@ -0,0 +1,17 @@
+Pedro. H@Pedro MINGW64 /c/desarrollo/educacionit/bootcamp-java-universidad-2023/desafios-bootcamp2023/website (main)
+$ git log
+commit dc15c9d45d309a084d21f66f6a3cc63cc42c3525 (HEAD -> main)
+Author: paht1995 <paht1995@gmail.com>
+Date:   Sun Jun 25 23:15:50 2023 -0300
+
+    Eliminación de status.js
+
+commit f3a0da5f633d7462d5586d8625efe80b9befa120 (origin/main, origin/HEAD)
+Author: paht1995 <paht1995@gmail.com>
+Date:   Sun Jun 25 21:13:43 2023 -0300
+
+    Creación status.js
+
+
+dejo el log de la creación y eliminación de status.js luego de haber hecho el git stash --incluede-untracked en la rama custom-navbar, esto no lo vi en el log, por eso lo detallo
+
